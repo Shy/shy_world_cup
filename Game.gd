332b42017ba62f3b41dbad23ff75d59d4f79fdcc
Joe Nash <1790822+joenash@users.noreply.github.com>
2024-03-18 17:08:34 +0100

    joe init

---
+++ Game.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var racer_scene = preload("res://racer.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _on_button_pressed():
+	pass
