28cf2fb944641514dc24296e0b9bd231e9125fd3
Shy Ruparel <Shy@users.noreply.github.com>
2024-03-25 11:58:58 -0400

    Random movement

--- Game.gd
+++ Game.gd
@@ -1,14 +1,23 @@
 extends Node3D
 
 @onready var racer_scene = preload("res://racer.tscn")
+@onready var track = $Track/StaticBody3D2/NavigationRegion3D/MeshInstance3D
+@onready var button = $Control/Button
+@onready var navAgent = $Racer/NavigationAgent3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	await get_tree().physics_frame
+	get_tree().call_group('racers','set_movement_target',Vector3(0,0,0))
+	randomize()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if navAgent.is_navigation_finished():
+		button.disabled = false
+	else:
+		button.disabled =  true
 	
 func _on_button_pressed():
-	pass
+	var new_vector = Vector3(randf_range(0, track.scale.x),0,randf_range(0, track.scale.z))
+	button.text = str(new_vector) 
+	get_tree().call_group('racers','set_movement_target',new_vector)
