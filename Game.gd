dd8510e7b3e3b2d11835234b594e48bad84eea99
Shy Ruparel <Shy@users.noreply.github.com>
2024-04-08 10:28:35 -0400

    Catchup

--- Game.gd
+++ Game.gd
@@ -2,22 +2,34 @@ extends Node3D
 
 @onready var racer_scene = preload("res://racer.tscn")
 @onready var track = $Track/StaticBody3D2/NavigationRegion3D/MeshInstance3D
+@onready var track_points = $Track/Path3D
 @onready var button = $Control/Button
-@onready var navAgent = $Racer/NavigationAgent3D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	await get_tree().physics_frame
-	get_tree().call_group('racers','set_movement_target',Vector3(0,0,0))
-	randomize()
+	for i in range (5):
+		var racer =  racer_scene.instantiate()
+		
+		racer.position = Vector3(i,0,0)
+		add_child(racer)
+		racer.has_arrived.connect(_on_has_arrived)
+		racer.set_movement_target(track_points.curve.get_point_position(racer.point_idx))
 
-func _process(delta):
-	if navAgent.is_navigation_finished():
-		button.disabled = false
-	else:
-		button.disabled =  true
-	
+func _process(_delta):
+	pass
+
 func _on_button_pressed():
-	var new_vector = Vector3(randf_range(0, track.scale.x),0,randf_range(0, track.scale.z))
-	button.text = str(new_vector) 
-	get_tree().call_group('racers','set_movement_target',new_vector)
+	var racers = get_tree().get_nodes_in_group('racers')
+	for racer in racers:
+		racer.point_idx = 0
+		racer.set_movement_target(track_points.curve.get_point_position(racer.point_idx))
+		print('Racer has been reset.')
+
+func _on_has_arrived(racer):
+	racer.point_idx += 1
+	if racer.point_idx < track_points.curve.point_count:
+		racer.set_movement_target(track_points.curve.get_point_position(racer.point_idx))
+		print("Racer has arrived")
+	else:
+		print("Racer finished")
--- racer.gd
+++ racer.gd
@@ -1,15 +1,19 @@
 extends CharacterBody3D
 
 var movement_speed: float = 2.0
-var movement_target_position: Vector3 = Vector3(2.0, 0.0, 1.0)
+var point_idx = 0
+var stopped = false
+
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+
+signal has_arrived()
 
 func _ready():
 	## These values need to be adjusted for the actor's speed
 	## and the navigation layout.
-	navigation_agent.path_desired_distance = 0.5
-	navigation_agent.target_desired_distance = 0.5
+	navigation_agent.path_desired_distance = 2
+	navigation_agent.target_desired_distance =2
 	## Make sure to not await during _ready.
 	#call_deferred("actor_setup")
 #
@@ -21,14 +25,22 @@ func _ready():
 	#set_movement_target(movement_target_position)
 
 func set_movement_target(movement_target: Vector3):
+	stopped = false
 	navigation_agent.set_target_position(movement_target)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if navigation_agent.is_navigation_finished():
-		print("Finished")
+		if !stopped:
+			stopped = true
+			has_arrived.emit(self)
 		return
-	print("Is navigation reached: ", navigation_agent.is_target_reached())
 	var current_agent_position: Vector3 = global_position
-	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	var next_location: Vector3 = navigation_agent.get_next_path_position()
+	velocity = (next_location - current_agent_position).normalized() * movement_speed
+	navigation_agent.set_velocity(velocity)
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity):
+	velocity = velocity.move_toward((safe_velocity),.25)
 	move_and_slide()
+
+	
